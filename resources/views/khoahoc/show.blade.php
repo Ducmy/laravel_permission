a3543a74d388cc0063d0e7ea7a40d6ae1e32692b
cnvs_my <mynguyen@Macpro-my.local>
2020-10-11 09:08:13 +0700

    Done rating

--- resources/views/khoahoc/show.blade.php
+++ resources/views/khoahoc/show.blade.php
@@ -2,7 +2,7 @@
 
 @push('css')
 <link href="{{ asset('css/khoahoc/index.css') }}" rel="stylesheet" />
-<link href="http://netdna.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.css" rel="stylesheet">
+<!-- <link href="http://netdna.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.css" rel="stylesheet"> -->
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-star-rating/4.0.2/css/star-rating.min.css" />
 <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
 <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700" rel="stylesheet">
